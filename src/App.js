17311fe106be3e9a71cb44a69bb0a8329df83aa9
Anthony <anthony.gilliam@gmail.com>
2019-09-14 21:33:28 -0400

    Creating Home Page and adding react-bootstrap for theme and layout [finish ch69]

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container, Row, Col, Button } from "react-bootstrap";
 import './App.css';
+import logo from './logo.jpg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <section className="App">
+            <header>
+                <h1>Task Master</h1>
+                <img src={logo} className="App-logo" alt="logo"/>
+            </header>
+            <main>
+                <Container fluid={true}>
+                    <Row noGutters={true}>
+                        <Button id='new-task' variant="primary">Create New Task</Button>
+                    </Row>
+                    <Row noGutters={true}>
+                        <h2>Monitor Tasks</h2>
+                    </Row>
+                </Container>
+            </main>
+        </section>
+    );
 }
 
 export default App;
