c9914a889dda0ce0113a3e7e4dcfa9e185b3d2d0
seerosecode <keshalakemusic@gmail.com>
2019-09-26 00:01:25 -0400

    connected create new task button on home page to from page via Link from react-router-dom. This is a temporary path to the form. Once the modal is created, we'll render the createNewTask component there

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 import { Container, Row, Col, Button } from "react-bootstrap";
 import MonitorTasks from './tasks/MonitorTasks'
+import { Link } from 'react-router-dom'
 
 class HomeContainer extends Component{
+
+  handleClick = () => {
+
+  }
+
   render(){
     return(
       <main>
           <Container fluid={true}>
               <Row noGutters={true}>
-                  <Button id='new-task' variant="primary">Create New Task</Button>
+                  <Button as={Link} to="/create-new-task" id='new-task' variant="primary" onClick={this.handleClick}>Create New Task</Button>
               </Row>
               <Row noGutters={true}>
                   <MonitorTasks />
