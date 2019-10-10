3bb387574e216e42d3124dbdbc1d11458eda4686
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-10-10 00:07:21 -0400

    add function to get time left over to display in progress bar

--- src/components/TaskDetails.js
+++ src/components/TaskDetails.js
@@ -4,6 +4,35 @@ import { Table, ProgressBar } from 'react-bootstrap'
 
 export default function TaskDetailView(props) {
     
+    // updateTime () => int 
+    //      returns percentag of time that has accured since the task was started
+    let updateTime = () => {
+        // Starttime and endtime should be in seconds for an accurate conversion
+        let { startTime, endTime } = props;
+
+        // 10 is the current time for testing purposed
+        let currentTime = 10;
+
+        // let currentTime = new Date().getSeconds()
+        let accuredTime = getAccuredTime(currentTime,startTime)
+        console.log(accuredTime)
+        let runTime = getRunTime(endTime,startTime)
+        console.log(runTime)
+        return ((accuredTime / runTime) * 100).toFixed(2)
+    }
+
+    // getAccuredTime (int endTime, int startTime) => int 
+    //      returns int of time that has past by
+    let getAccuredTime = (currentTime,startTime) => {
+        return currentTime > startTime ?   currentTime - startTime : 24 - startTime + currentTime ;
+    }
+
+    // getRunTime (int endTime, int startTime) => int
+    //      returns int of runTime
+    let getRunTime = (endTime,startTime) => {
+        return endTime > startTime ? endTime - startTime : 24 - startTime + endTime
+    }
+
 
     return (
         <div>
@@ -29,7 +58,7 @@ export default function TaskDetailView(props) {
                     </tr>
                     <tr>
                         <td className="tg-0lax" colspan="2">
-                            <ProgressBar  label={percent} now={percent} />
+                            <ProgressBar  className="tg-progressBar" label={`${updateTime()}%`} now={updateTime()} />
                         </td>
                     </tr>
                 </tbody>
